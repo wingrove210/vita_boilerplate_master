ccc8b77c30871530d122a96c756f19b8e772a221
wingrove210 <vladidermkov330@gmail.com>
2024-12-04 16:07:14 +0300

    new file:   .gitignore 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.tsx 	new file:   src/main.tsx 	new file:   src/pages/About.tsx 	new file:   src/pages/Welcome.tsx 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+export const About = () => {
+    return (
+        <h1>
+            ABOUT PAGE
+        </h1>
+    );
+};
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,19 @@
+import {useSearchParams} from 'react-router-dom';
+
+export const Welcome = () => {
+    const [searchParams] = useSearchParams();
+
+    const id = searchParams.get('id');
+
+    return (
+        <div>
+            <h1>
+                id = {id}
+            </h1>
+            <h1>
+                Welcome PAGE
+            </h1>
+        </div>
+
+    );
+};
